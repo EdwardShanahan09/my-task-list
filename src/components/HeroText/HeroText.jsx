208154a1f232598d845c34ca75a38c93de3fb103
Edward Shanahan <edwardshanahan79@gmail.com>
2024-08-05 14:54:02 +0100

    Style HeroText component for mobile and tablet and large screens

--- src/components/HeroText/HeroText.jsx
+++ src/components/HeroText/HeroText.jsx
@@ -3,9 +3,17 @@ import Logo from "../../assets/icons/logo.svg";
 const HeroText = () => {
   return (
     <div className="text-center">
-      <img src={Logo} alt="My Task List Logo" height={50} width={50} />
-      <h1>My Task List</h1>
-      <p>Keep track of your daily tasks!</p>
+      <div className="flex justify-center items-center mb-2 md:mb-4">
+        <img
+          className="h-8 w-8 md:h-12 md:w-12"
+          src={Logo}
+          alt="My Task List Logo"
+        />
+      </div>
+      <h1 className="font-bold text-2xl mb-2 md:mb-4 md:text-4xl">
+        My Task List
+      </h1>
+      <p className="text-sm italic">Keep track of your daily tasks!</p>
     </div>
   );
 };
