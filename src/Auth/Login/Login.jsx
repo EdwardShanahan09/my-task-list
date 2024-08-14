f6022261c5fdebfd45fd8f7a30d3288ea6f3b7ef
Edward Shanahan <edwardshanahan79@gmail.com>
2024-08-14 17:35:49 +0100

    Add elements to Login component

--- src/Auth/Login/Login.jsx
+++ src/Auth/Login/Login.jsx
@@ -1,5 +1,57 @@
+import { useState } from "react";
+import InputField from "../../components/InputField/InputField";
+import { Link } from "react-router-dom";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
 const Login = () => {
-  return <h1>Login Page</h1>;
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div>
+      <h2 className="font-bold text-lg mb-2 md:mb-4 md:text-3xl">Login</h2>
+      <form className="mb-4">
+        <InputField
+          labelId="email"
+          labelName="Email"
+          type="email"
+          name="email"
+          onChange={handleChange}
+        />
+
+        <InputField
+          labelId="password"
+          labelName="Password"
+          type="password"
+          name="password"
+          onChange={handleChange}
+        />
+
+        <button className="w-full py-2 px-4 mt-4 bg-indigo-600 text-white font-semibold rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+          Login
+        </button>
+      </form>
+
+      <p className="text-sm italic text-right">
+        Don't have an account?{" "}
+        <Link className="text-indigo-600" to="/">
+          Sign Up
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default Login;
