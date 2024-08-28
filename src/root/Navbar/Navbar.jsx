5c11ff64f320d78dc1a22cd85e172a8eec7e7cf3
Edward Shanahan <edwardshanahan79@gmail.com>
2024-08-28 19:32:12 +0100

    Logout user in the dashboard

--- src/root/Navbar/Navbar.jsx
+++ src/root/Navbar/Navbar.jsx
@@ -1,3 +1,6 @@
+import { UserContext } from "../../context/UserContext";
+import { useContext } from "react";
+import { logoutUser } from "../../lib/auth/auth";
 import AvatarIcon from "../../assets/icons/avatar.svg";
 import TodayIcon from "../../assets/icons/today-icon.svg";
 import TodaysIcon from "../../assets/icons/all-icon.svg";
@@ -5,6 +8,13 @@ import CategoryIcon from "../../assets/icons/category-icon.svg";
 import SignoutIcon from "../../assets/icons/sign-out-icon.svg";
 
 const Navbar = () => {
+  const { setCurrentUser } = useContext(UserContext);
+
+  const logout = () => {
+    logoutUser();
+    setCurrentUser(false);
+  };
+
   return (
     <nav className="flex md:flex-col md:h-full md:w-20 md:fixed md:left-0 md:top-0 md:bottom-0 bg-white shadow-md fixed bottom-0 left-0 right-0 p-4">
       <div className="flex items-center justify-between md:flex-col">
@@ -35,7 +45,7 @@ const Navbar = () => {
       </ul>
 
       <div className="mt-4 md:mt-auto">
-        <button>
+        <button onClick={logout}>
           <img className="w-5" src={SignoutIcon} alt="Signout Icon" />
           <span className="hidden">Logout</span>
         </button>
