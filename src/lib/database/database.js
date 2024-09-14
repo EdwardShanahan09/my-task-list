c874d04ebe7a5c98b6fccbbbcb60bdc5a298a69a
Edward Shanahan <edwardshanahan79@gmail.com>
2024-09-14 18:58:10 +0100

    List user's categories

--- src/lib/database/database.js
+++ src/lib/database/database.js
@@ -1,21 +1,37 @@
 import { database, id, Permission, Role } from "../config";
+import { Query } from "appwrite";
 
 const DATABASE_ID = import.meta.env.VITE_DATABASE_ID;
 const CATEGORY_COLLECTION_ID = import.meta.env.VITE_CATEGORY_COLLECTION_ID;
 const TASK_COLLECTION_ID = import.meta.env.VITE_TASK_COLLECTION_ID;
 
-export const createCategory = async (title, userID) => {
+export const createCategory = async (title, userId) => {
   try {
     const newCategory = await database.createDocument(
       DATABASE_ID,
       CATEGORY_COLLECTION_ID,
       id.unique(),
-      { title },
-      [Permission.read(Role.user(userID)), Permission.write(Role.user(userID))]
+      { title, userId },
+      [Permission.read(Role.user(userId)), Permission.write(Role.user(userId))]
     );
     return newCategory;
   } catch (error) {
     console.log("Failed to create category", error);
+    throw error;
+  }
+};
+
+export const listCategories = async (userId) => {
+  try {
+    const response = await database.listDocuments(
+      DATABASE_ID,
+      CATEGORY_COLLECTION_ID,
+      [Query.equal("userId", userId)]
+    );
+
+    return response.documents;
+  } catch (error) {
+    console.error("Faild to list categories", error);
     throw error;
   }
 };
--- src/root/Categories/Categories.jsx
+++ src/root/Categories/Categories.jsx
@@ -1,29 +1,54 @@
-import { useState, useContext } from "react";
-import { createCategory } from "../../lib/database/database";
+import { useState, useContext, useEffect } from "react";
+import { createCategory, listCategories } from "../../lib/database/database";
 import { UserContext } from "../../context/UserContext";
 
 const Categories = () => {
   const [category, setCategory] = useState("");
   const { currentUser } = useContext(UserContext);
+  const [categories, setCategories] = useState([]);
 
-  console.log(currentUser);
+  const fetchCategories = async () => {
+    if (!currentUser) return;
+
+    try {
+      const fetchedCategories = await listCategories(currentUser.$id);
+
+      setCategories(fetchedCategories);
+    } catch (error) {
+      console.log("Failed to fetch categories", error);
+      alert("Failed to fetch categories");
+    }
+  };
 
   const handleNewCategory = async () => {
     try {
       await createCategory(category, currentUser.$id);
+      setCategory("");
+      fetchCategories();
     } catch (error) {
       console.log(error);
       alert(error);
     }
   };
 
+  useEffect(() => {
+    fetchCategories();
+  }, [currentUser]);
+
   return (
     <div>
       <input
         onChange={(event) => setCategory(event.target.value)}
         type="text"
+        value={category}
       />
       <button onClick={handleNewCategory}>Add Category</button>
+
+      <ul>
+        {categories.map((category) => (
+          <li key={category.$id}>{category.title}</li>
+        ))}
+      </ul>
     </div>
   );
 };
